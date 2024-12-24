c73c611f858a95cef59293fbbbc6dca706cfd825
LuisDev2003 <villegasalazar01@gmail.com>
2024-12-24 10:39:59 -0500

    Agregar archivos de estilo y scripts globales, e implementar el menú de encabezado

---
+++ assets/scripts/header.js
@@ -0,0 +1,16 @@
+import { $ } from "./global";
+
+const toggleHeaderMenu = $("#toggle-header-menu");
+const headerMenu = $("#header-menu");
+
+function toggleMenu() {
+  if (toggleHeaderMenu.dataset.status === "open") {
+    toggleHeaderMenu.dataset.status = "close";
+    headerMenu.style.display = "block";
+  } else {
+    toggleHeaderMenu.dataset.status = "open";
+    headerMenu.style.display = "none";
+  }
+}
+
+toggleHeaderMenu.addEventListener("click", toggleMenu);
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="./images/favicon.svg" type="image/svg+xml" />
+
+    <title>Electrocopper Riojas</title>
+
+    <link rel="stylesheet" href="./assets/styles/global.css" />
+    <link rel="stylesheet" href="./assets/styles/header.css" />
+  </head>
+  <body>
+    
+    <?php require_once "./layout/header.php" ?>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+      sapiente.
+    </p>
+  </body>
+
+  <script type="module" src="./assets/scripts/header.js"></script>
+</html>
