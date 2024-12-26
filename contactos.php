4a6c9e173df82efa39cf572b4eb4d49a54765a1a
LuisDev2003 <villegasalazar01@gmail.com>
2024-12-26 17:31:02 -0500

    Agregar página de contactos con estilos y contenido

---
+++ contactos.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <?php require_once "./layout/meta.html" ?>
+
+    <title>Electrocopper Riojas - Contactos</title>
+
+    <link rel="stylesheet" href="./assets/styles/contactos.css" />
+  </head>
+  <body>
+    <?php require_once "./layout/header.html" ?>
+
+    <section class="s-1">
+      <h1 class="title">Contactos</h1>
+    </section>
+
+    <section class="s-2">
+      <div class="d-1">
+        <div>
+          <h4 class="title">Head Office</h4>
+
+          <h3 class="title">SD</h3>
+
+          <h5 class="title">South Dakota</h5>
+          <h5 class="title">+1-800-123-4567</h5>
+          <h5 class="title">36/B W 1st Ave, Miller, SD 57362, USA</h5>
+        </div>
+
+        <div>
+          <img src="images/city-free-img.jpg" alt="Ubicación" />
+        </div>
+      </div>
+
+      <div class="d-1">
+        <h4 class="title">Regional Offices</h4>
+
+        <div>
+          <h3 class="title">MT</h3>
+
+          <h5 class="title">Montana</h5>
+          <h5 class="title">19-A/2 3rd Ave, Grey Park, MT 57362, USA</h5>
+          <h5 class="title">+1-800-123-4567</h5>
+        </div>
+
+        <div>
+          <h3 class="title" style="margin-top: 50px">ND</h3>
+
+          <h5 class="title">North Dakota</h5>
+          <h5 class="title">24/C W 2nd Ave, James Rd, ND 12345, USA</h5>
+          <h5 class="title">+1-800-123-4567</h5>
+        </div>
+      </div>
+    </section>
+
+    <?php require_once "./layout/footer.html" ?>
+  </body>
+
+  <script type="module" src="./assets/scripts/header.js"></script>
+</html>
