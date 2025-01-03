74928cb9797d5e134f31b4123a9424bc224bf316
LuisDev2003 <villegasalazar01@gmail.com>
2025-01-02 19:59:06 -0500

    Agregar sección de héroe en la página principal; mejorar estilos y estructura de secciones

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ $servicios = $servicio->getAll(); ?>
           </button>
 
           <nav id="nav-menu" class="menu">
+            <a href="./#" class="item">Inicio</a>
             <a href="./#servicios" class="item">Servicios</a>
             <a href="./#sobre-nosotros" class="item">Sobre nosotros</a>
             <a href="./#contactos" class="item">Contactos</a>
@@ -90,6 +91,9 @@ $servicios = $servicio->getAll(); ?>
     <aside id="sidebar-menu" class="sidebar">
       <ul class="menu">
         <li>
+          <a href="./#" class="item">Inicio</a>
+        </li>
+        <li>
           <a href="./#servicios" class="item">Servicios</a>
         </li>
         <li>
@@ -105,6 +109,57 @@ $servicios = $servicio->getAll(); ?>
     </aside>
 
     <main class="main">
+      <section class="hero">
+        <div class="wrapper">
+          <div>
+            <h3 class="title">Electrocopper Riojas</h3>
+
+            <h3 class="title">¡Soluciones Eléctricas Rápidas y Seguras!</h3>
+
+            <p class="description">
+              Reparaciones, instalaciones y mantenimiento eléctrico para hogares
+              y empresas.
+            </p>
+
+            <a
+              href="https://api.whatsapp.com/send?phone=604982792"
+              type="button"
+              target="_blank"
+              class="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="icon"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                <path
+                  d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"
+                />
+              </svg>
+
+              <span>Contáctanos</span>
+            </a>
+          </div>
+
+          <div>
+            <img
+              src="./images/logo.png"
+              alt="Logo de la empresa"
+              class="image"
+            />
+          </div>
+        </div>
+      </section>
+
       <section id="servicios" class="services">
         <ul>
           <?php foreach ($servicios as $item): ?>
