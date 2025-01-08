daea8bb61b023474be78bec6abc3dd31d8b3324e
LuisDev2003 <villegasalazar01@gmail.com>
2025-01-08 17:23:15 -0500

    Agregar tablas para empresas, personas, clientes, cotizaciones y detalle de cotizaciones en la base de datos

--- database/tables.sql
+++ database/tables.sql
@@ -26,6 +26,51 @@ CREATE TABLE empleados (
 ) ENGINE = InnoDB;
 
 -- ###################################################################
+CREATE TABLE empresas (
+	empresa_id  	INT AUTO_INCREMENT  PRIMARY KEY,
+    ruc				CHAR(11)		    NOT NULL,
+    razon_social	VARCHAR(255)	    NOT NULL,
+    correo			VARCHAR(255)	    NOT NULL,
+    telefono		VARCHAR(20)		    NOT NULL,
+    direccion		TEXT				NULL,
+    created_at 		DATETIME			DEFAULT NOW(),
+    updated_at		DATETIME			NULL,
+    inactive_at		DATETIME			NULL,
+
+    CONSTRAINT un_ruc_emp			UNIQUE(ruc),
+    CONSTRAINT un_razon_social_emp	UNIQUE(razon_social),
+    CONSTRAINT un_correo_emp		UNIQUE(correo),
+    CONSTRAINT un_telefono_emp		UNIQUE(telefono)
+) ENGINE = InnoDB;
+
+-- ###################################################################
+CREATE TABLE personas (
+    persona_id		INT AUTO_INCREMENT	PRIMARY KEY,
+    nombres			VARCHAR(50)			NOT NULL,
+    apellidos		VARCHAR(50)			NOT NULL,
+    created_at 		DATETIME			DEFAULT NOW(),
+    updated_at		DATETIME			NULL,
+    inactive_at		DATETIME			NULL
+) ENGINE = InnoDB;
+
+-- ###################################################################
+CREATE TABLE clientes (
+    cliente_id		INT AUTO_INCREMENT		PRIMARY KEY,
+    persona_id	    INT					    NULL,
+    empresa_id	    INT					    NULL,
+    tipo		    ENUM('natural', 'juridica')	NOT NULL,
+    created_at 		DATETIME				DEFAULT NOW(),
+    updated_at		DATETIME				NULL,
+    inactive_at		DATETIME				NULL,
+
+    CONSTRAINT un_persona_cli		UNIQUE(persona_id),
+    CONSTRAINT un_empresa_cli		UNIQUE(empresa_id),
+    CONSTRAINT fk_persona_cli		FOREIGN KEY (persona_id) REFERENCES personas(persona_id),
+    CONSTRAINT fk_empresa_cli		FOREIGN KEY (empresa_id) REFERENCES empresas(empresa_id)
+) ENGINE = InnoDB;
+
+
+-- ###################################################################
 CREATE TABLE servicios (
 	servicio_id		INT AUTO_INCREMENT	PRIMARY KEY,
     nombre			VARCHAR(120)		NOT NULL,
@@ -72,4 +117,31 @@ CREATE TABLE comentarios (
     created_at 		DATETIME			DEFAULT NOW(),
     updated_at		DATETIME			NULL,
     inactive_at		DATETIME			NULL
+) ENGINE = InnoDB;
+
+-- ###################################################################
+CREATE TABLE cotizaciones (
+	cotizacion_id	INT AUTO_INCREMENT  PRIMARY KEY,
+    codigo			VARCHAR(15)		    NULL,
+    cliente_id		INT				    NULL,
+    empleado_id		INT				    NULL,
+    created_at 		DATETIME			DEFAULT NOW(),
+    updated_at		DATETIME			NULL,
+    inactive_at		DATETIME			NULL,
+
+    CONSTRAINT fk_cliente_cot		FOREIGN KEY (cliente_id)	REFERENCES clientes(cliente_id),
+    CONSTRAINT fk_empleado_cot		FOREIGN KEY (empleado_id)	REFERENCES empleados(empleado_id)
+) ENGINE = InnoDB;
+
+-- ###################################################################
+CREATE TABLE detalle_cotizaciones (
+	detalle_cotizacion_id   INT	AUTO_INCREMENT  PRIMARY KEY,
+    cotizacion_id	        INT				    NOT NULL,
+    servicio_id		        INT				    NOT NULL,
+    created_at 		        DATETIME			DEFAULT NOW(),
+    updated_at		        DATETIME			NULL,
+    inactive_at		        DATETIME			NULL,
+
+    CONSTRAINT fk_cotizacion_d_cot	FOREIGN KEY (cotizacion_id)	REFERENCES cotizaciones(cotizacion_id),
+    CONSTRAINT fk_servicio_d_cot	FOREIGN KEY (servicio_id)	REFERENCES servicios(servicio_id)
 ) ENGINE = InnoDB;
