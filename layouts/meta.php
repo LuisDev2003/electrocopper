3f0c779b568df4183f6926e5ebebd6a7083e84d5
LuisDev2003 <villegasalazar01@gmail.com>
2025-01-08 16:46:23 -0500

    Actualizar imagen de vista previa en metadatos y simplificar la estructura del código

--- layouts/meta.php
+++ layouts/meta.php
@@ -15,10 +15,7 @@
   property="og:description"
   content="Expertos en instalaciones, reparaciones y mantenimiento eléctrico en Calle de san fidel 54 – piso 1C - Madrid. Contáctanos para un servicio confiable."
 />
-<meta
-  property="og:image"
-  content="./images/cropped-ELECTROCOPPER-logo-web_page-0001-1.jpg"
-/>
+<meta property="og:image" content="./images/logo-2.jpg" />
 <meta property="og:url" content="https://preview.electrocopperriojas.com" />
 <meta property="og:type" content="website" />
 <link rel="canonical" href="https://preview.electrocopperriojas.com" />
