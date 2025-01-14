37a06b02059ea10ca1b34b4d550f35a43e885a0c
LuisDev2003 <villegasalazar01@gmail.com>
2025-01-14 10:07:40 -0500

    Agregar formulario de selección de servicios en la página de "Presupuesto"; mejorar estilos y estructura de contenido

--- layouts-2/budget.php
+++ layouts-2/budget.php
@@ -28,63 +28,188 @@
       <ul id="contents" class="contents">
         <li data-content-index="1" class="content active">
           <div>
-            <h3 class="question">¿En que te podemos ayudar?</h3>
+            <h3 class="question">1. ¿En que te podemos ayudar?</h3>
 
-            <div class="answer">Lorem ipsum dolor sit amet.</div>
+            <div class="answer">
+              <form id="select-services" class="form">
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 1"
+                  />
+                  <span>Servicio 1</span>
+                </label>
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 2"
+                  />
+                  <span>Servicio 2</span>
+                </label>
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 3"
+                  />
+                  <span>Servicio 3</span>
+                </label>
+              </form>
+            </div>
 
-            <div data-next-step="2" class="actions">
-              <button class="button prev">Anterior</button>
-              <button class="button next">Siguiente</button>
+            <div class="actions">
+              <button type="submit" form="select-services" class="button next">
+                Siguiente
+              </button>
             </div>
           </div>
         </li>
         <li data-content-index="2" class="content">
           <div>
-            <h3 class="question">¿Cuando empezamos con tu proyecto?</h3>
+            <h3 class="question">2. ¿Cuando empezamos con tu proyecto?</h3>
 
-            <div class="answer">Lorem ipsum dolor sit amet.</div>
+            <div class="answer">
+              <form id="select-date" class="form">
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 1"
+                  />
+                  <span>En menos de 3 meses</span>
+                </label>
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 2"
+                  />
+                  <span>En más de 3 meses</span>
+                </label>
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 3"
+                  />
+                  <span>Estoy planificando, no tengo fecha prevista</span>
+                </label>
+              </form>
+            </div>
 
-            <div data-next-step="3" class="actions">
-              <button class="button prev">Anterior</button>
-              <button class="button next">Siguiente</button>
+            <div class="actions">
+              <button type="button" class="button prev">Anterior</button>
+              <button type="button" class="button next">Siguiente</button>
             </div>
           </div>
         </li>
         <li data-content-index="3" class="content">
           <div>
-            <h3 class="question">¿Cual es tu presupuesto?</h3>
+            <h3 class="question">3. ¿Cual es tu presupuesto?</h3>
 
-            <div class="answer">Lorem ipsum dolor sit amet.</div>
+            <div class="answer">
+              <form id="select-money" class="form">
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 1"
+                  />
+                  <span>Hasta 5.000€</span>
+                </label>
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 2"
+                  />
+                  <span>Entre 5.000€ y 10.000€</span>
+                </label>
+                <label>
+                  <input
+                    required
+                    type="radio"
+                    name="service"
+                    value="Servicio 3"
+                  />
+                  <span>Más de 10.000€</span>
+                </label>
+              </form>
+            </div>
 
-            <div data-next-step="4" class="actions">
-              <button class="button prev">Anterior</button>
-              <button class="button next">Siguiente</button>
+            <div class="actions">
+              <button type="button" class="button prev">Anterior</button>
+              <button type="button" class="button next">Siguiente</button>
             </div>
           </div>
         </li>
         <li data-content-index="4" class="content">
           <div>
             <h3 class="question">
-              Para seguir con tus ideales dejanos tus datos
+              4. Para seguir con tus ideales dejanos tus datos
             </h3>
 
-            <div class="answer">Lorem ipsum dolor sit amet.</div>
+            <div class="answer">
+              <form id="select-date" class="form">
+                <input
+                  required
+                  type="text"
+                  name="nombres"
+                  placeholder="Nombre"
+                  class="input"
+                />
 
-            <div data-next-step="5" class="actions">
-              <button class="button prev">Anterior</button>
-              <button class="button next">Siguiente</button>
+                <input
+                  required
+                  type="text"
+                  name="telefono"
+                  placeholder="Teléfono"
+                  class="input"
+                />
+
+                <input
+                  required
+                  type="text"
+                  name="correo"
+                  placeholder="Correo eletrónico"
+                  class="input"
+                />
+              </form>
+            </div>
+
+            <div class="actions">
+              <button type="button" class="button prev">Anterior</button>
+              <button type="button" class="button next">Siguiente</button>
             </div>
           </div>
         </li>
         <li data-content-index="5" class="content">
           <div>
-            <h3 class="question">Danos los detalles de tu proyecto</h3>
+            <h3 class="question">5. Danos los detalles de tu proyecto</h3>
 
-            <div class="answer">Lorem ipsum dolor sit amet.</div>
+            <div class="answer">
+              <form id="select-date" class="form">
+                <textarea
+                  rows="5"
+                  name="mensaje"
+                  placeholder="Mensaje"
+                  class="textarea"
+                ></textarea>
+              </form>
+            </div>
 
             <div class="actions">
-              <button class="button prev">Anterior</button>
-              <button class="button next">Siguiente</button>
+              <button type="button" class="button prev">Anterior</button>
             </div>
           </div>
         </li>
--- scripts-2/budget.js
+++ scripts-2/budget.js
@@ -1,9 +1,47 @@
-import { $ } from "./index.js";
+import { $, $$ } from "./index.js";
 
-const $steps = $("#steps");
-const $contents = $("#contents");
+const $prevButtons = $$(".button.prev[type='button']");
+const $nextButtons = $$(".button.next[type='button']");
+const $formSelectService = $("#select-services");
 
-console.log({
-  $steps,
-  $contents,
+const switchStepContent = (currentIndex, targetIndex) => {
+  const $currentStep = $(`[data-step-index="${currentIndex}"]`);
+  const $targetStep = $(`[data-step-index="${targetIndex}"]`);
+
+  const $currentContent = $(`[data-content-index="${currentIndex}"]`);
+  const $targetContent = $(`[data-content-index="${targetIndex}"]`);
+
+  $currentStep.classList.remove("active");
+  $targetStep.classList.add("active");
+
+  $currentContent.classList.remove("active");
+  $targetContent.classList.add("active");
+};
+
+const createContentChangeHandler = (direction) => (event) => {
+  const $content = event.target.closest(".content");
+
+  const { contentIndex } = $content.dataset;
+  const newIndex = +contentIndex + direction;
+
+  switchStepContent(+contentIndex, newIndex);
+};
+
+$prevButtons.forEach((button) => {
+  button.addEventListener("click", createContentChangeHandler(-1));
+});
+
+$nextButtons.forEach((button) => {
+  button.addEventListener("click", createContentChangeHandler(1));
+});
+
+$formSelectService.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const service = formData.get("service");
+
+  console.log(service);
+
+  if (service) createContentChangeHandler(1)(event);
 });
