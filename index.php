2da9a729b88c398946c808633e255ffdacb8bc41
LuisDev2003 <villegasalazar01@gmail.com>
2025-01-20 11:55:41 -0500

    Agregar configuración de Tailwind CSS y mejorar el diseño del encabezado

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Electrocopper Riojas</title>
 
-    <link rel="stylesheet" href="./styles-2/global.css" />
-    <link rel="stylesheet" href="./styles-2/index.css" />
-    <link rel="stylesheet" href="./styles-2/hero.css" />
-    <link rel="stylesheet" href="./styles-2/home.css" />
-  </head>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Electrocopper Riojas</title>
 
-  <body>
-    <?php require_once "./layouts-2/header.php" ?>
+  <link rel="stylesheet" href="./styles-2/global.css" />
+  <link rel="stylesheet" href="./styles-2/index.css" />
+  <link rel="stylesheet" href="./styles-2/hero.css" />
+  <link rel="stylesheet" href="./styles-2/home.css" />
+  <link rel="stylesheet" href="./styles/output.css">
+</head>
 
-    <main class="main">
-      <?php require_once "./layouts-2/hero.php" ?>
+<body>
+  <?php require_once "./layouts-2/header.php" ?>
 
-      <?php require_once "./layouts-2/home-services.php" ?>
+  <main class="main">
+    <?php require_once "./layouts-2/hero.php"; ?>
 
-      <?php require_once "./layouts-2/brands.php" ?>
-    </main>
+    <?php require_once "./layouts-2/home-services.php" ?>
 
-    <?php require_once "./layouts-2/footer.php" ?>
-  </body>
+    <?php require_once "./layouts-2/brands.php" ?>
+  </main>
 
-  <script type="module" src="./scripts-2/header.js"></script>
+  <?php require_once "./layouts-2/footer.php" ?>
+</body>
+
+<script type="module" src="./scripts-2/header.js"></script>
+
 </html>
